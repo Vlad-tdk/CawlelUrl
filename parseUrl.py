afc822c312c570bbb37727803a35691a8ba4bdf3
Vlad <xalif80@gmail.com>
2024-03-19 21:44:47 +0100

    Update parseUrl.py

--- parseUrl.py
+++ parseUrl.py
@@ -5,7 +5,7 @@ import colorama
 import argparse
 import sys
 
-# инициализация модуля colorama
+# initialize colorama module
 colorama.init()
 
 GREEN = colorama.Fore.GREEN
@@ -13,135 +13,135 @@ GRAY = colorama.Fore.LIGHTBLACK_EX
 RESET = colorama.Fore.RESET
 YELLOW = colorama.Fore.YELLOW
 
-# инициализация множеств ссылок (уникальные ссылки)
-ссылки_внутренние = set()
-ссылки_внешние = set()
+# initialize sets of links (unique links)
+internal_links = set()
+external_links = set()
 
-посещенные_ссылки = 0
+visited_links = 0
 
 
 def is_valid_url(url):
     """
-    Проверяет, является ли `url` допустимым URL-адресом.
+    Checks whether `url` is a valid URL.
     """
     parsed = urlparse(url)
     return bool(parsed.netloc) and bool(parsed.scheme)
 
 
-def извлечь_все_ссылки_с_веб_сайта(url):
+def extract_all_links_from_website(url):
     """
-    Возвращает все URL-адреса, найденные на `url`, которые принадлежат тому же веб-сайту.
+    Returns all URLs found on `url` belonging to the same website.
     """
-    # все URL-адреса `url`
-    ссылки = set()
-    # доменное имя URL без протокола
-    доменное_имя = urlparse(url).netloc
-    # инициализация HTTP-сессии
-    сессия = HTMLSession()
+    # all URLs of `url`
+    links = set()
+    # domain name of the URL without the protocol
+    domain_name = urlparse(url).netloc
+    # initialize an HTTP session
+    session = HTMLSession()
     try:
-        # совершаем HTTP-запрос и получаем ответ
-        ответ = сессия.get(url)
-        # выполнение JavaScript
+        # make HTTP request & retrieve response
+        response = session.get(url)
+        # execute JavaScript
         try:
-            ответ.html.render()
+            response.html.render()
         except:
             pass
-        soup = BeautifulSoup(ответ.html.html, "html.parser")
+        soup = BeautifulSoup(response.html.html, "html.parser")
         for a_tag in soup.find_all("a"):
             href = a_tag.get("href")
             if href == "" or href is None:
-                # пустой тег href
+                # empty href tag
                 continue
-            # объединяем URL, если это относительная ссылка (не абсолютная ссылка)
+            # join the URL if it's relative (not absolute link)
             href = urljoin(url, href)
             parsed_href = urlparse(href)
-            # удаляем параметры GET URL, фрагменты URL и т. д.
+            # remove URL GET parameters, URL fragments, etc.
             href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path
             if not is_valid_url(href):
-                # не допустимый URL-адрес
+                # not a valid URL
                 continue
-            if href in ссылки_внутренние:
-                # уже есть в множестве
+            if href in internal_links:
+                # already in the set
                 continue
-            if доменное_имя not in href:
-                # внешняя ссылка
-                if href not in ссылки_внешние:
-                    print(f"{GRAY}[!] Внешняя ссылка: {href}{RESET}")
-                    ссылки_внешние.add(href)
+            if domain_name not in href:
+                # external link
+                if href not in external_links:
+                    print(f"{GRAY}[!] External link: {href}{RESET}")
+                    external_links.add(href)
                 continue
-            print(f"{GREEN}[*] Внутренняя ссылка: {href}{RESET}")
-            ссылки.add(href)
-            ссылки_внутренние.add(href)
+            print(f"{GREEN}[*] Internal link: {href}{RESET}")
+            links.add(href)
+            internal_links.add(href)
     except Exception as e:
-        print(f"{YELLOW}[!] Ошибка при извлечении ссылок с {url}: {e}{RESET}")
+        print(f"{YELLOW}[!] Error extracting links from {url}: {e}{RESET}")
 
 
-def обход_веб_страницы(url, максимальное_количество_ссылок=30):
+def crawl_web_page(url, max_urls=30):
     """
-    Обходит веб-страницу и извлекает все ссылки.
-    Вы найдете все ссылки в глобальных переменных множества `ссылки_внешние` и `ссылки_внутренние`.
-    параметры:
-        максимальное_количество_ссылок (int): максимальное количество URL-адресов для обхода, по умолчанию 30.
+    Crawls a web page and extracts all links.
+    You'll find all links in global variables `external_links` and `internal_links`.
+    params:
+        max_urls (int): number of max urls to crawl, default is 30.
     """
-    global посещенные_ссылки
-    посещенные_ссылки += 1
-    print(f"{YELLOW}[*] Обход: {url}{RESET}")
-    извлечь_все_ссылки_с_веб_сайта(url)
-    if посещенные_ссылки > максимальное_количество_ссылок:
+    global visited_links
+    visited_links += 1
+    print(f"{YELLOW}[*] Crawling: {url}{RESET}")
+    extract_all_links_from_website(url)
+    if visited_links > max_urls:
         return
-    ссылки = ссылки_внутренние.copy()  # копируем множество ссылок, чтобы не изменять его во время обхода
-    for link in ссылки:
-        обход_веб_страницы(link, максимальное_количество_ссылок)
+    links = internal_links.copy()  # copy the set of links to avoid changing it during crawling
+    for link in links:
+        crawl_web_page(link, max_urls)
 
 
-def main(url, максимальное_количество_ссылок=30):
-    обход_веб_страницы(url, максимальное_количество_ссылок)
-    print("[+] Общее количество внутренних ссылок:", len(ссылки_внутренние))
-    print("[+] Общее количество внешних ссылок:", len(ссылки_внешние))
-    print("[+] Общее количество URL-адресов:", len(ссылки_внешние) + len(ссылки_внутренние))
-    print("[+] Общее количество просмотренных URL-адресов:", максимальное_количество_ссылок)
+def main(url, max_urls=30):
+    crawl_web_page(url, max_urls)
+    print("[+] Total Internal links:", len(internal_links))
+    print("[+] Total External links:", len(external_links))
+    print("[+] Total URLs:", len(external_links) + len(internal_links))
+    print("[+] Total crawled URLs:", max_urls)
 
-    доменное_имя = urlparse(url).netloc
+    domain_name = urlparse(url).netloc
 
-    # сохраняем внутренние ссылки в файл
+    # save the internal links to a file
     try:
-        with open(f"{доменное_имя}_внутренние_ссылки.txt", "w") as f:
-            for internal_link in ссылки_внутренние:
+        with open(f"{domain_name}_internal_links.txt", "w") as f:
+            for internal_link in internal_links:
                 print(internal_link.strip(), file=f)
     except Exception as e:
-        print(f"{YELLOW}[!] Ошибка при сохранении внутренних ссылок: {e}{RESET}")
+        print(f"{YELLOW}[!] Error saving internal links: {e}{RESET}")
 
-    # сохраняем внешние ссылки в файл
+    # save the external links to a file
     try:
-        with open(f"{доменное_имя}_внешние_ссылки.txt", "w") as f:
-            for external_link in ссылки_внешние:
+        with open(f"{domain_name}_external_links.txt", "w") as f:
+            for external_link in external_links:
                 print(external_link.strip(), file=f)
     except Exception as e:
-        print(f"{YELLOW}[!] Ошибка при сохранении внешних ссылок: {e}{RESET}")
+        print(f"{YELLOW}[!] Error saving external links: {e}{RESET}")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Инструмент извлечения ссылок с помощью Python")
-    parser.add_argument("file", help="Путь к файлу со списком сайтов.")
-    parser.add_argument("-m", "--max-urls", help="Максимальное количество URL-адресов для обхода, по умолчанию 30.", default=30, type=int)
+    parser = argparse.ArgumentParser(description="Link Extractor Tool with Python")
+    parser.add_argument("file", help="Path to the file containing the list of websites.")
+    parser.add_argument("-m", "--max-urls", help="Number of max URLs to crawl, default is 30.", default=30, type=int)
     
     args = parser.parse_args()
     file_path = args.file
-    максимальное_количество_ссылок = args.max_urls
+    max_urls = args.max_urls
 
     try:
         with open(file_path, "r") as file:
             for line in file:
                 site_url = line.strip()
-                print(f"{YELLOW}[*] Обработка сайта: {site_url}{RESET}")
-                ссылки_внутренние.clear()
-                ссылки_внешние.clear()
-                посещенные_ссылки = 0
-                main(site_url, максимальное_количество_ссылок)
-                print()  # Добавляем пустую строку между обработанными сайтами
+                print(f"{YELLOW}[*] Processing website: {site_url}{RESET}")
+                internal_links.clear()
+                external_links.clear()
+                visited_links = 0
+                main(site_url, max_urls)
+                print()  # Add a blank line between processed websites
     except FileNotFoundError:
-        print(f"{YELLOW}[!] Файл со списком сайтов не найден: {file_path}{RESET}")
+        print(f"{YELLOW}[!] File with the list of websites not found: {file_path}{RESET}")
         sys.exit(1)
     except Exception as e:
-        print(f"{YELLOW}[!] Произошла ошибка при чтении файла: {e}{RESET}")
+        print(f"{YELLOW}[!] Error reading file: {e}{RESET}")
         sys.exit(1)
